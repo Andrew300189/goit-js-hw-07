aedfbf473b163825acf0b9683d72b542633bb400
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-07-31 21:54:15 +0300

    Changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,66 +1,71 @@
+// 01-gallery.js
 import { galleryItems } from './gallery-items.js';
-import * as galleryItemModule from './gallery-items.js';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+// Функция для создания элемента галереи
 function createGalleryItem(item) {
-const galleryItemElement = document.createElement('li'); // Changed variable name to avoid conflicts
-galleryItemElement.classList.add('gallery__item');
-
-const link = document.createElement('a');
-link.classList.add('gallery__link');
-link.href = item.original;
-
-const image = document.createElement('img');
-image.classList.add('gallery__image');
-image.src = item.preview;
-image.alt = item.description;
-image.setAttribute('data-source', item.original);
-
-link.appendChild(image);
-galleryItemElement.appendChild(link);
-
-return galleryItemElement;
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `;
 }
 
 const gallery = document.querySelector('.gallery');
 
+// Рендерим элементы галереи
 galleryItems.forEach((item) => {
-const galleryItemElement = createGalleryItem(item);
-gallery.appendChild(galleryItemElement);
+  const galleryItemElement = document.createElement('li');
+  galleryItemElement.innerHTML = createGalleryItem(item);
+  gallery.appendChild(galleryItemElement);
 });
 
+// Инициализируем переменную для хранения текущего экземпляра модального окна
+let currentLightbox;
+
+// Открываем модальное окно при клике на изображении
 gallery.addEventListener('click', (event) => {
-event.preventDefault();
+  event.preventDefault();
 
-if (event.target.nodeName === 'IMG') {
-const imageUrl = event.target.dataset.source;
+  if (event.target.classList.contains('gallery__image')) {
+    const imageUrl = event.target.dataset.source;
 
+    // Закрываем предыдущий экземпляр модального окна перед созданием нового
+    if (currentLightbox) {
+      currentLightbox.close();
+    }
 
-const modalImage = document.querySelector('.modal__image');
-    modalImage.src = imageUrl;
+    // Создаем новый экземпляр модального окна
+    const lightbox = basicLightbox.create(`
+      <img src="${imageUrl}" alt="Image">
+    `, {
+      closable: true,
+      onShow: (instance) => {
+        document.addEventListener('keydown', handleKeyPress);
+      },
+      onClose: (instance) => {
+        document.removeEventListener('keydown', handleKeyPress);
+      },
+    });
 
-    const modal = document.querySelector('.modal');
-    modal.style.display = 'flex';
+    // Сохраняем ссылку на текущий экземпляр модального окна
+    currentLightbox = lightbox;
 
-    document.addEventListener('keydown', handleKeyPress);
+    lightbox.show();
   }
 });
 
-
+// Функция для закрытия модального окна по нажатию клавиши Escape
 function handleKeyPress(event) {
-  if (event.key === 'Escape') {
-    closeModal();
+  if (event.key === 'Escape' && currentLightbox) {
+    currentLightbox.close();
   }
 }
-
-function closeModal() {
-  const modal = document.querySelector('.modal');
-  modal.style.display = 'none';
-  document.removeEventListener('keydown', handleKeyPress);
-}
-
-
-const modal = document.querySelector('.modal');
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    closeModal();
-  }
-});
